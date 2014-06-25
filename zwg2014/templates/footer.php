99c0a8b27bf292561e9f348622b4e30d0019352d
mclxly <mclxly@gmail.com>
2014-06-25 21:13:49 +0800

    小修改

--- zwg2014/templates/footer.php
+++ zwg2014/templates/footer.php
@@ -4,7 +4,7 @@
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>&copy; 2012-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>
       <br>Built with <a href="http://roots.io/starter-theme/">Roots</a> and <a href="http://wordpress.org/">WordPress</a>. 
-      <br>本网站基于Responsive设计,已做手机/iPad测试,如果建议及浏览问题请联系我
+      <br>本网站基于Responsive设计,已做手机/iPad测试,如果有建议及浏览问题请联系我
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/email.png" width="135" height="21" alt="email" title="email">
       <!-- The source code of this theme is <a href="https://github.com/mclxly/wp-theme/tree/master/zwg2014">hosted on GitHub</a>. -->
       </p>
