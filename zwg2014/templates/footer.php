cb60bfc2ca9712fc9f755017e8f169a2988f5def
mclxly <mclxly@gmail.com>
2014-06-27 11:18:15 +0800

    增加Google统计

--- zwg2014/templates/footer.php
+++ zwg2014/templates/footer.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>&copy; 2012-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>
-      <br>Built with <a href="http://roots.io/starter-theme/">Roots</a> and <a href="http://wordpress.org/">WordPress</a>. 
+      <br>Built with <a href="http://roots.io/starter-theme/">Roots</a> and <a href="http://wordpress.org/">WordPress</a>.
       <br>本网站基于Responsive设计,已做手机/iPad测试,如果有建议及浏览问题请联系我
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/email.png" width="135" height="21" alt="email" title="email">
       <!-- The source code of this theme is <a href="https://github.com/mclxly/wp-theme/tree/master/zwg2014">hosted on GitHub</a>. -->
@@ -15,6 +15,7 @@
 
 <?php wp_footer(); ?>
 
+<!-- website analytics begin -->
 <div class="hide">
 <script type="text/javascript">
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
@@ -22,4 +23,13 @@ document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3
 </script>
 </div>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-34671632-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- website analytics end -->
