c187a77ad75f8acde03f098a6c5bf3c77bf14e8e
mclxly <mclxly@gmail.com>
2014-07-08 14:32:55 +0800

    添加地理位置信息

--- zwg2014/templates/footer.php
+++ zwg2014/templates/footer.php
@@ -3,7 +3,10 @@
     <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
       <p>&copy; 2012-<?php echo date('Y'); ?> <?php bloginfo('name'); ?>
-      <br>Built with <a href="http://roots.io/starter-theme/">Roots</a> and <a href="http://wordpress.org/">WordPress</a>.
+        [<a href="http://j.map.baidu.com/FVQht" target="_blank">百度</a>
+        <span class="octicon octicon-location"></span>
+        <a href="http://amap.com/0B96kC" target="_blank">高德</a>]
+      <br>Built with <a href="http://roots.io/starter-theme/" target="_blank">Roots</a> and <a href="http://wordpress.org/" target="_blank">WordPress</a>.
       <br>本网站基于Responsive设计,已做手机/iPad测试,如果有建议及浏览问题请联系我
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/email.png" width="135" height="21" alt="email" title="email">
       <!-- The source code of this theme is <a href="https://github.com/mclxly/wp-theme/tree/master/zwg2014">hosted on GitHub</a>. -->
