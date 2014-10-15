cfd97171b1ecec00b89c9795b8a509aede1957f7
mclxly <mclxly@gmail.com>
2014-10-15 22:25:07 +0800

    add tag list

--- zwg2014/templates/entry-meta.php
+++ zwg2014/templates/entry-meta.php
@@ -1,2 +1,10 @@
-<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-<p class="byline author vcard"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
+<p class="byline author vcard">
+  <time class="published" datetime="<?php echo get_the_time('c'); ?>">
+  <?php echo get_the_date(); ?>
+  </time>
+  <?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a>
+  <span style="tag-list">
+  <?php the_terms( $post->ID, 'category', '标签: ', ' / ' ); ?>
+  </span>
+  moment-with-locales.min.js
+</p>
